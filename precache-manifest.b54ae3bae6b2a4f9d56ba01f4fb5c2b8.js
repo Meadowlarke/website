38e001717f6cabb9d0910974eae2fda34fb9af1c
biniona <biniona@uchicago.edu>
2020-02-21 21:38:33 -0500

    Updates

---
+++ precache-manifest.b54ae3bae6b2a4f9d56ba01f4fb5c2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1645d79c7d0a6b196451a7cf4c5c4f",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "6c281800569f31af00d3",
+    "url": "/website/static/css/main.fff48254.chunk.css"
+  },
+  {
+    "revision": "c40a06796695b5bb4e56",
+    "url": "/website/static/js/2.04a44480.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/website/static/js/2.04a44480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c281800569f31af00d3",
+    "url": "/website/static/js/main.b7240961.chunk.js"
+  },
+  {
+    "revision": "6ca7e6b6ec808c32ac93",
+    "url": "/website/static/js/runtime-main.f6286eea.js"
+  }
+]);
